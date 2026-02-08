a22c70bbb598b041203edce7f0788dbd6e8985ee
pavan <pavan9964666@gmail.com>
2026-02-08 22:45:15 +0530

    Updated API URL for production

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -574,7 +574,7 @@ IEA*1*000000001~`;
 export default App;
 
 // OLD (line ~200-210):
-   const res = await fetch('http://localhost:5000/api/generate-856', {
+   const res = await fetch("http://localhost:5000/api/generate-856", {
    
    // NEW:
-   const res = await fetch('https://edi-suite-backend.onrender.com/api/generate-856', {
+   const res = await fetch('https://edi-validator-generator-react-node-js.onrender.com', {
